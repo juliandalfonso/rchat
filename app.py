994e68d7e546f5e09b2ae90ea8a10eeb3d5dbcf6
Hecodes98 <julian.cardona@utp.edu.co>
2019-12-12 11:26:39 -0500

    Connect to database

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template
 
 from wtform_fields import *
+from models import *
 
-
-#Configure app
+# Configure app
 app = Flask(__name__)
 app.secret_key = 'replace later'
+
+# Configure db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tvduqqvnejtcxa:e8eb77ff53c7b0b4ddb07fd495815186243466a012ef03b24984ea342b4fabc8@ec2-107-22-224-154.compute-1.amazonaws.com:5432/d9ghma237tt9pc'
+
+db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     reg_form = RegistrationForm()
-    if reg_form.validate_on_submit(): 
-        return "Great success"
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Nombre de usuario ya existente, porfavor elige otro."
+        # Add user to database
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Insert into DB"
+
     return render_template('index.html', form=reg_form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- wtform_fields.py
+++ wtform_fields.py
@@ -2,21 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
+
 class RegistrationForm(FlaskForm):
     """Registration form"""
-    username = StringField('username_label', 
-    validators=[InputRequired(message="Nombre de usuario requerido"), 
-    Length(min=4, max=25, 
-    message="El nombre de usuario debe tener entre 4 a 25 caracteres")])
+    username = StringField('username_label',
+                           validators=[InputRequired(message="Nombre de usuario requerido"),
+                                       Length(min=4, max=25,
+                                              message="El nombre de usuario debe tener entre 4 a 25 caracteres")])
 
     password = PasswordField('password_label',
-    validators=[InputRequired(message="La contraseña es requerida"), 
-    Length(min=4, max=25, 
-    message="La contraseña debe tener entre 4 a 25 caracteres")])
+                             validators=[InputRequired(message="La contraseña es requerida"),
+                                         Length(min=4, max=25,
+                                                message="La contraseña debe tener entre 4 a 25 caracteres")])
 
     confirm_pswd = PasswordField('confirm_pswd_label',
-    validators=[InputRequired(message="La contraseña es requerida"), 
-    EqualTo('password', message="Las contraseñas deben coincidir.")])
+                                 validators=[InputRequired(message="La contraseña es requerida"),
+                                             EqualTo('password', message="Las contraseñas deben coincidir.")])
 
     submit_button = SubmitField('Create')
-    
